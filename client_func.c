40e164631ca3de829340e7bb3c2dda8885e5e46f
licg <sofunbaba@gmail.com>
2017-11-18 16:47:30 +0800

    handle all the alloc memory and free when exit.

--- client_func.c
+++ client_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <unistd.h>
 #include <event2/event.h>
 #include <event2/util.h>
@@ -7,78 +8,188 @@
 #include <event2/bufferevent.h>
 
 #include "server.h"
-#include "client_func.h"
 #include "util.h"
+#include "client_func.h"
+#include "cmd_process.h"
 
 /*
- * read task from the client.
+ * the global client info list.
+ */
+struct list_head gl_client_info;
+
+static bool verify_pkg(char *pkg, size_t len)
+{
+    bool ret = true;
+    ev_uint8_t opt = 0, args[100];
+    ev_uint32_t head, session, cmd, arglen;
+    ev_uint16_t pkglen;
+
+    assert(pkg != NULL);
+
+    pkg_parse_data(pkg, &head, PKG_FIELD_HEAD);
+    ret &= (head==DEFAULT_CMD_HEAD);
+
+    pkg_parse_data(pkg, &session, PKG_FIELD_SESSION);
+
+    pkg_parse_data(pkg, &pkglen, PKG_FIELD_PKGLEN);
+    ret &= (pkglen==len);
+
+    pkg_parse_data(pkg, &cmd, PKG_FIELD_CMD);
+    pkg_parse_data(pkg, &opt, PKG_FIELD_OPT);
+    arglen = pkg_parse_data(pkg, args, PKG_FIELD_ARGS);
+
+    log_msg(E_DEBUG, "Head: 0x%08x, Session: 0x%08x, pkglen: 0x%04x, cmd: 0x%08x, opt: 0x%02x, arglen: %d, args: %s", head, session, pkglen, cmd, opt, arglen, args);
+
+    log_msg(E_DEBUG, "ret: %d", ret);
+
+    return ret;
+}
+
+/*
+ * read cmd from the client.
  */
 void client_read_cb(struct bufferevent *bev, void *arg)
 {
-    ev_uint8_t ret = 0;
-    char *task = NULL, *task_p = NULL;
+    char *pkg = NULL;
     size_t len = 0;
-    struct evbuffer *input  = bufferevent_get_input(bev);
-    struct evbuffer *output = bufferevent_get_output(bev);
-    evutil_socket_t fd      = bufferevent_getfd(bev);
+    struct client_info *cinfo = arg;
+    struct evbuffer *input    = bufferevent_get_input(bev);
 
     /*
-     * read task that end with '\r\n' or '\r' or '\n'
+     * read cmd that end with '\r\n' or '\r' or '\n'
      */
-    task = evbuffer_readln(input, &len, EVBUFFER_EOL_ANY);
+    pkg = evbuffer_readln(input, &len, EVBUFFER_EOL_ANY);
     if(len > 0)
     {
-        log_msg(E_DEBUG, "Client:%d got a message, len:%lu", fd, len);
+        log_msg(E_DEBUG, "Client:%d got a message, len:%lu", cinfo->fd, len);
 
-        if(len == DEFAULT_TASK_LEN)
+        if(verify_pkg(pkg, len))
         {
-            if(task[0] == DEFAULT_TASK_HEAD)
-            {
-                task_p = bin2hex(task, len);
-                log_msg(E_DEBUG, "recv:%s", task_p);
-                free(task_p);
-            }
+            log_msg(E_ALL, "recv:%s", pkg);
+
+            process_cmd(pkg, cinfo);
         }
 
-        free(task);
+        free(pkg);
     }
 }
 
 void client_error_cb(struct bufferevent *bev, short what, void *arg)
 {
-    struct event_base *base = bufferevent_get_base(bev);
-    evutil_socket_t fd = bufferevent_getfd(bev);
+    struct client_info *cinfo = arg;
     int err = EVUTIL_SOCKET_ERROR();
 
     if(what & BEV_EVENT_EOF)
-        log_msg(E_DEBUG, "Client:%d exit.", fd);
+        log_msg(E_DEBUG, "Client:%d exit.", cinfo->fd);
     else if(what & BEV_EVENT_ERROR)
-        log_msg(E_ERROR, "Client:%d got a error(%s).", fd, evutil_socket_error_to_string(err));
+        log_msg(E_ERROR, "Client:%d got a error(%s).", cinfo->fd, evutil_socket_error_to_string(err));
 
-    bufferevent_free(bev);
-    event_base_loopexit(base, NULL);
+    event_base_loopexit(cinfo->base, NULL);
+}
+
+void list_client_info_free(struct client_info *cinfo)
+{
+    list_node_t *np = NULL;
+
+    for(np=gl_client_info.head; np; np=np->next)
+        if(np->private_data == cinfo)
+            list_del(&gl_client_info, np);
+
+    log_msg(E_DEBUG, "Delete a client.(remain:%d)", gl_client_info.length);
+}
+
+void list_client_info_add(struct client_info *cinfo)
+{
+    list_add(&gl_client_info, cinfo);
 }
 
 void *client_func(void *arg)
 {
-    struct bufferevent *bev = NULL;
-    struct event_base *base = NULL;
+    struct client_info *cinfo = arg;
 
-    evutil_socket_t fd = *(evutil_socket_t *)arg;
+    log_msg(E_DEBUG, "Create thread fd:%d", cinfo->fd);
 
-    log_msg(E_DEBUG, "Create thread fd:%d", fd);
+    cinfo->base = event_base_new();
 
-    base = list_event_base_new();
+    cinfo->bev = bufferevent_socket_new(cinfo->base, cinfo->fd, BEV_OPT_CLOSE_ON_FREE);
+    bufferevent_setcb(cinfo->bev, client_read_cb, NULL, client_error_cb, cinfo);
+    bufferevent_enable(cinfo->bev, EV_READ);
 
-    bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-    bufferevent_setcb(bev, client_read_cb, NULL, client_error_cb, NULL);
-    bufferevent_enable(bev, EV_READ);
+    /*
+     * fill the client info.
+     */
+    memset(cinfo->client_name, 0, sizeof(cinfo->client_name));
+    cinfo->uptime = 0;
 
-    event_base_dispatch(base);
+    /*
+     * add the client info to the list.
+     */
+    list_client_info_add(cinfo);
 
-    log_msg(E_DEBUG, "Thread client fd:%d exit!", fd);
+    event_base_dispatch(cinfo->base);
 
-    free(arg);
-    list_event_base_free(&gl_event_base, base);
+    log_msg(E_DEBUG, "Thread client fd:%d exit!", cinfo->fd);
+
+    close(cinfo->fd);
+    bufferevent_free(cinfo->bev);
+    event_base_free(cinfo->base);
+    list_client_info_free(cinfo);
+
+    return NULL;
 }
+
+struct pkg_field pkg_field_data[PKG_FIELD_MAX] = {
+    {0, 4},
+    {8, 4},
+    {16, 2},
+    {20, 4},
+    {28, 1},
+    {30, -1},
+};
+
+ev_uint16_t pkg_get_arglen(char *pkg)
+{
+    ev_uint16_t pkglen = 0;
+
+    pkg_parse_data(pkg, &pkglen, PKG_FIELD_PKGLEN);
+    pkglen -= pkg_field_data[PKG_FIELD_ARGS].position;
+
+    return (pkglen<=0) ? 0 : pkglen;
+}
+
+ev_uint16_t pkg_parse_data(char *pkg, void *field, pkg_field_type_t type)
+{
+    ev_uint32_t *u32p = field;
+    ev_uint16_t *u16p = field;
+
+    if(field != NULL)
+    {
+        if(pkg_field_data[type].size == -1)
+        {
+            ev_uint16_t arglen = pkg_get_arglen(pkg);
+            if(arglen != 0)
+                strncpy(field, pkg+pkg_field_data[type].position, arglen);
+            *(ev_uint8_t *)(field+arglen) = '\0';
+
+            return arglen;
+
+        }
+        else
+        {
+            hex2bin((ev_uint8_t *)field, pkg+pkg_field_data[type].position, pkg_field_data[type].size);
+            if(pkg_field_data[type].size == sizeof(int))
+                *u32p = ntohl(*u32p);
+            else if(pkg_field_data[type].size == sizeof(short))
+                *u16p = ntohs(*u16p);
+            else
+            {
+            }
+
+        }
+    }
+        return 0;
+
+    return pkg_field_data[type].size;
+}
+
 
--- client_func.h
+++ client_func.h
@@ -1,15 +1,87 @@
 #ifndef _CLIENT_FUNC_H_
 #define _CLIENT_FUNC_H_
 
-#define DEFAULT_TASK_LEN  54
-#define DEFAULT_TASK_HEAD 0x5a
+#include <event2/util.h>
+#include "list.h"
 
+/*
+ * Transport every field as Byte
+ * eg: "1A2BB2A101020304001E0000001000"
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * request  pkgs = head(4) + session(4) + pkglen(2) + cmd(4) + opt(1) + args(n) + "\r\n"
+ *
+ * response pkgs = head(4) + session(4) + pkglen(2) + result(n) + "\r\n"
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * head:             0x1a2bb2a1
+ * request session:  it could be a random num.
+ * response session: must be request session + 1.
+ * pkglen:           total pkgs length but "\r\n".
+ * cmd:              function that what to execute.
+ * opt:              read or write operate.
+ * args:             args of the cmd.
+ * result:           the request return data.
+ *
+ * ----------------------------------------------------------------------------
+ */
 
-struct client_info {
+#define DEFAULT_CLIENT_CMD_MAX  0xff
+#define DEFAULT_CLIENT_CMD_MASK 0xabcddcbaul
 
+#define DEFAULT_CMD_HEAD         0x1a2bb2a1ul
+
+#define remote_cmd(cmd) (DEFAULT_CLIENT_CMD_MASK^cmd)
+#define local_cmd remote_cmd
+#define install_client_cmd(cmd, func) do{gl_client_cmd[local_cmd[cmd]] = func;}while(0)
+
+typedef enum {
+    PKG_FIELD_HEAD,
+    PKG_FIELD_SESSION,
+    PKG_FIELD_PKGLEN,
+    PKG_FIELD_CMD,
+    PKG_FIELD_OPT,
+    PKG_FIELD_ARGS,
+    PKG_FIELD_MAX,
+}pkg_field_type_t;
+
+struct pkg_field {
+    ev_uint8_t position;
+    ev_int8_t  size;
 };
 
+struct client_info {
+    pthread_t pt;
+    evutil_socket_t fd;
+    char client_name[100];
+    ev_uint64_t uptime;
+    struct bufferevent *bev;
+    struct sockaddr_in sin;
+    struct event_base *base;
+};
+
+typedef ev_uint8_t (*client_cmd_handle)(struct client_info *, ev_uint8_t, void *, ev_uint32_t);
+
+
+
+/*
+ * the global client info list.
+ */
+extern struct list_head gl_client_info;
+extern struct pkg_field pkg_filed_data[PKG_FIELD_MAX];
+
+
 void *client_func(void *arg);
+void list_client_info_free(struct client_info *cinfo);
+void list_client_info_add(struct client_info *cinfo);
+
+
+/*
+ * parse pkg filed.
+ */
+ev_uint16_t pkg_parse_data(char *pkg, void *field, pkg_field_type_t type);
 
 
 
